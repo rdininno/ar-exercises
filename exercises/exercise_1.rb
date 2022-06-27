7ec2e79c1434495c115a4a10acd34d77ef01b341
Robert Di Ninno <robertdininno@gmail.com>
2022-06-27 23:53:40 +0000

    exercise 1

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -8,4 +8,5 @@ store1 = Store.create(name: "Buranby", annual_revenue: 300000, mens_apparel: tru
 store2 = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
 store3 = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
 
+
 puts Store.count
