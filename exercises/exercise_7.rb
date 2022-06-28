a0aa4353a114709b5ed3f8fc1bcebe228289039d
Robert Di Ninno <robertdininno@gmail.com>
2022-06-28 00:37:49 +0000

    changes exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,9 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts Employee.create(:first_name => nil).valid?
+puts Employee.create(:last_name => nil).valid?
+puts Employee.create(:hourly_rate => 50).valid?
+puts Employee.create(:hourly_rate => 15).valid?
+puts Employee.create(:store_id => 2).valid?
+puts Employee.create(:store_id => nil).valid?
